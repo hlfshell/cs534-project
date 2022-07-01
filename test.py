5b38fb51283bc84474f932e59f0a7c657663ba5a
keith <kchester@gmail.com>
2022-07-01 13:31:54 -0700

    fix: proof of multi-run

--- test.py
+++ test.py
@@ -2,12 +2,17 @@
 
 from traffic_agent.sumo import Simulation
 
-simulation = Simulation("./sim/test.sumocfg")
-simulation.start()
+stats = []
 
-while not simulation.complete():
-    simulation.step()
+for i in range(0,5):
+    simulation = Simulation("./sim/test.sumocfg")
+    simulation.start()
 
-simulation.stop()
-print(simulation.get_stats())
-simulation.shutdown()
+    while not simulation.complete():
+        simulation.step()
+
+    simulation.stop()
+    stats.append(simulation.get_stats())
+    simulation.shutdown()
+
+print(stats)
