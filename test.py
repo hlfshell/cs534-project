258c6171d577ac9ce2355506ade5be2360469f14
keith <kchester@gmail.com>
2022-07-02 11:29:43 -0700

    fix: use chicago sim

--- test.py
+++ test.py
@@ -5,7 +5,8 @@ from traffic_agent.sumo import Simulation
 stats = []
 
 for i in range(0,5):
-    simulation = Simulation("./sim/test.sumocfg")
+    # simulation = Simulation("./sim/test.sumocfg")
+    simulation = Simulation("./chicago/chicago.sumocfg")
     simulation.start()
 
     while not simulation.complete():
